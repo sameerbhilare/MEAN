25bf5856adfccd510c1b203b9eb826b098453bcd
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-28 18:57:33 +0530

    Implemented CORS - to allow cross origin requests to backend

--- Workspace/mean-course/backend/app.js
+++ Workspace/mean-course/backend/app.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+// cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, DELETE, PUT, PATCH"
+  );
+  next();
+});
+
 app.use("/api/posts", (req, res, next) => {
   const posts = [
     { id: "asad1212", title: "First Post", content: "First post content" },
