ffb293d2e565d5d1ee4de8b9179a4dd42c362f3d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-28 17:34:00 +0530

    API to fetch posts

--- Workspace/mean-course/backend/app.js
+++ Workspace/mean-course/backend/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    { id: "asad1212", title: "First Post", content: "First post content" },
+    { id: "asad1213", title: "Second Post", content: "Second post content" },
+  ];
 
-app.use((req, res, next) => {
-  res.send("Hello from Express");
+  res.status(200).json({
+    message: "Posts Fetched successfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
