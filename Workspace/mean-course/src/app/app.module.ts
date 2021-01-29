7a0ecd73a185becd96572bf625b331678492ded6
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-29 11:58:09 +0530

    Added routes for root (/) and create post

--- Workspace/mean-course/src/app/app.module.ts
+++ Workspace/mean-course/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatInputModule,
