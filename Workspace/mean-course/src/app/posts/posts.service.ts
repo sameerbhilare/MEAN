ad73238fa4cdee425adefd4a0f0383bf924a3404
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-29 11:06:34 +0530

    Transform response data in get request ( _id to id)

--- Workspace/mean-course/src/app/posts/posts.service.ts
+++ Workspace/mean-course/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 
 @Injectable({
@@ -19,11 +20,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postsResponse) => {
+          return postsResponse.posts.map((post) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id, // tranforming "_id" to "id"
+            };
+          });
+        })
       )
-      .subscribe((postsResponse) => {
-        this.posts = postsResponse.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
