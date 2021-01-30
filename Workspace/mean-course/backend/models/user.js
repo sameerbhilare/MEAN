f6c1032d8d5be7a7415aa0957b4b34d252bdd759
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-30 20:04:50 +0530

    Implemented backend route for signup request

--- Workspace/mean-course/backend/models/user.js
+++ Workspace/mean-course/backend/models/user.js
@@ -7,6 +7,8 @@ const userSchema = mongoose.Schema({
 });
 
 /*
+  We cannot depend upon mongoose' unique: true as it is used to do some internal optimizations
+  and we cannot rely on it. Hence using mongoose-unique-validator plugin
   Unique validator is a plugin that will simply add an extra hook
   that checks your data before it saves it to the database.
 */
--- Workspace/mean-course/backend/routes/user.routes.js
+++ Workspace/mean-course/backend/routes/user.routes.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
 const router = express.Router();
 
 // signup
-router.post("/signup", (req, res, next) => {});
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash, // save hashed password
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "User successfully created.",
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
+      });
+  });
+});
 
 module.exports = router;
