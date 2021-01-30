045aff791a69c44d71a6bdd1d4bb9ef12457bf2d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-30 19:35:59 +0530

    Created Signup form

--- Workspace/mean-course/src/app/auth/signup/signup.component.ts
+++ Workspace/mean-course/src/app/auth/signup/signup.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  isLoading: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSignup(signupForm: NgForm) {
+    console.log(signupForm.value);
   }
-
 }
