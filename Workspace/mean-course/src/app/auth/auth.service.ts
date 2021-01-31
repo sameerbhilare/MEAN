acbf473e6ab2170aeebfdc6dc17db60c303fc6f8
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-01-31 14:33:11 +0530

    Added Auth Interceptor to paa Authorization header in each outgoing http request

--- Workspace/mean-course/src/app/auth/auth.service.ts
+++ Workspace/mean-course/src/app/auth/auth.service.ts
@@ -6,7 +6,13 @@ import { AuthData } from './auth-data.model';
   providedIn: 'root',
 })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email, password };
@@ -20,9 +26,10 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email, password };
     this.http
-      .post('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
       .subscribe((response) => {
         console.log(response);
+        this.token = response.token;
       });
   }
 }
