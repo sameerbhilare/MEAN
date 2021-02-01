6ae68c1b581e631fa618f8de9311dd83c06e57b6
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-01 10:22:23 +0530

    Improving UI header to reflect Authentication Status

--- Workspace/mean-course/src/app/auth/auth.service.ts
+++ Workspace/mean-course/src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +9,16 @@ import { AuthData } from './auth-data.model';
 export class AuthService {
   private token: string;
 
+  private authStatusListener = new Subject<boolean>();
+
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -30,6 +37,7 @@ export class AuthService {
       .subscribe((response) => {
         console.log(response);
         this.token = response.token;
+        this.authStatusListener.next(true);
       });
   }
 }
--- Workspace/mean-course/src/app/header/header.component.ts
+++ Workspace/mean-course/src/app/header/header.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  authStatusListenerSubs: Subscription;
+
+  isUserAuthenticated: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authStatusListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.isUserAuthenticated = isAuthenticated;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.authStatusListenerSubs) {
+      this.authStatusListenerSubs.unsubscribe();
+    }
+  }
+}
