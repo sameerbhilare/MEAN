7ae7fdd7ac585792668c792d531f1aff2b108aca
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-01 19:00:33 +0530

    Added Error Dialog

--- Workspace/mean-course/src/app/error.interceptor.ts
+++ Workspace/mean-course/src/app/error.interceptor.ts
@@ -5,14 +5,20 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { ErrorComponent } from './error/error.component';
 
 /**
  * For every outgoing HTTP request, this interceptor will be called.
  * And if we get any error, we can handle in the catchError() below.
  */
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(public dialog: MatDialog) {}
+
   // insert 'Authorization' header in each http request
   intercept(
     req: HttpRequest<any>,
@@ -21,7 +27,12 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         console.log(error);
-        alert(error.error.message);
+        let errorMessage = 'An unknown error occurred!';
+        if (error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialog.open(ErrorComponent, { data: { message: errorMessage } });
+
         // throw this error because the error should be handled by the caller (somewhere in servie or component)
         return throwError(error);
       })
