e85938213e51f12fbf2bebc5cf9c227cc2eef53c
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-01 12:42:25 +0530

    Storing and using the token in the localstorage, auto login and auto logout based on expiration time

--- Workspace/mean-course/src/app/auth/auth.service.ts
+++ Workspace/mean-course/src/app/auth/auth.service.ts
@@ -51,12 +51,40 @@ export class AuthService {
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           const expiresIn = response.expiresIn;
-          this.timer = setTimeout(() => {
-            this.logout();
-          }, expiresIn * 1000);
+          this.autoLogoutUserTimer(expiresIn);
+
+          // save auth data
+          const expirationTime = new Date(
+            new Date().getTime() + expiresIn * 1000
+          );
+
+          this.saveAuthData(this.token, expirationTime);
+
           this.router.navigate(['/']);
         }
       });
+  }
+
+  /**
+   * initialize our auth status whenever the app starts
+   */
+  autoLoginUser() {
+    const authData = this.getAuthData();
+    if (!authData) {
+      return;
+    }
+
+    const expirationTime = authData.expirationTime;
+    const difference = expirationTime.getTime() - new Date().getTime();
+
+    if (difference > 0) {
+      console.log('User Auto logged in');
+      // token is NOT expired yet
+      this.token = authData.token;
+      this.isAuthenticated = true;
+      this.autoLogoutUserTimer(difference / 1000);
+      this.authStatusListener.next(true);
+    }
   }
 
   logout() {
@@ -64,7 +92,47 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
-    this.router.navigate(['/']);
+    this.clearAuthData();
     clearTimeout(this.timer);
+    this.router.navigate(['/']);
+  }
+
+  private autoLogoutUserTimer(durationInSeconds: number) {
+    console.log('Auto logout in ' + durationInSeconds + ' seconds');
+    this.timer = setTimeout(() => {
+      this.logout();
+    }, durationInSeconds * 1000);
+  }
+
+  /**
+   * local storage simply is a storage managed by the browser, accessible through Javascript,
+   * therefore vulnerable to cross-site scripting attacks.
+   * But Angular prevents us against these by default
+   * so we can't output script tags with Angular for example, so we should be safe .
+   */
+  private saveAuthData(token: string, expirationTime: Date) {
+    localStorage.setItem('token', token);
+    // NOT toString but toISoString which is serialized and standard style version of the date
+    // which we then can use to recreate it.
+    localStorage.setItem('expirationTime', expirationTime.toISOString());
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationTimeStr = localStorage.getItem('expirationTime');
+
+    if (!token || !expirationTimeStr) {
+      return;
+    }
+
+    return {
+      token: token,
+      expirationTime: new Date(expirationTimeStr),
+    };
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
   }
 }
