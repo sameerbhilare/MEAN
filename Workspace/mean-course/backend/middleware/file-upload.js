328e162162777643438e5f5969c5cc0724ff1e89
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-02 10:21:01 +0530

    Seperating middlewares

---
+++ Workspace/mean-course/backend/middleware/file-upload.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+
+const MIME_TYPES_MAP = {
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+  "image/png": "png",
+};
+
+// initialize multer storage - to store images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPES_MAP[file.mimetype];
+    let error = null;
+    if (!isValid) {
+      error = new Error("Invalid mime type");
+    }
+    cb(error, "backend/images");
+  },
+
+  // this filename will be set be multer and will be available at req.file.filename
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPES_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+module.exports = multer({ storage: storage }).single("image");
--- Workspace/mean-course/backend/routes/posts.routes.js
+++ Workspace/mean-course/backend/routes/posts.routes.js
@@ -1,50 +1,15 @@
 const express = require("express");
-const multer = require("multer");
 const checkAuth = require("../middleware/check-auth");
 const postController = require("../controllers/post.controller");
+const extractFile = require("../middleware/file-upload");
 
 const router = express.Router();
 
-const MIME_TYPES_MAP = {
-  "image/jpeg": "jpeg",
-  "image/jpg": "jpg",
-  "image/png": "png",
-};
-
-// initialize multer storage - to store images
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPES_MAP[file.mimetype];
-    let error = null;
-    if (!isValid) {
-      error = new Error("Invalid mime type");
-    }
-    cb(error, "backend/images");
-  },
-
-  // this filename will be set be multer and will be available at req.file.filename
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPES_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  },
-});
-
 // POST - create a post
-router.post(
-  "",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  postController.createPost
-);
+router.post("", checkAuth, extractFile, postController.createPost);
 
 // UPDATE a post
-router.put(
-  "/:id",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  postController.updatePost
-);
+router.put("/:id", checkAuth, extractFile, postController.updatePost);
 
 // GET a post
 router.get("/:id", postController.getPost);
