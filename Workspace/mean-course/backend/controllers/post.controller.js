a69bbed0bd5d04cf2f00cb044dc995c730270f9b
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-02 11:47:28 +0530

    Minor fix for update bug - error while updating if there is no real change

--- Workspace/mean-course/backend/controllers/post.controller.js
+++ Workspace/mean-course/backend/controllers/post.controller.js
@@ -41,7 +41,7 @@ exports.updatePost = (req, res, next) => {
     { title: req.body.title, content: req.body.content, imagePath: imagePath }
   )
     .then((result) => {
-      if (result.nModified > 0) {
+      if (result.n > 0) {
         res.status(200).json({
           message: "Post updated successfully.",
           creator: req.userData.userId, // saved in the checkAuth middleware
