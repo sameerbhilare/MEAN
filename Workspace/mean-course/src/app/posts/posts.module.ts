7449def425b229dc1abf81d722cc4bb7326bb9c2
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-02 14:31:20 +0530

    Fixed bug: including Router module as we are using routerLink directive in the post-list component

--- Workspace/mean-course/src/app/posts/posts.module.ts
+++ Workspace/mean-course/src/app/posts/posts.module.ts
@@ -5,10 +5,16 @@ import { PostListComponent } from './post-list/post-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '../angular-material.module';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PostCreateComponent, PostListComponent],
-  imports: [CommonModule, ReactiveFormsModule, AngularMaterialModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    AngularMaterialModule,
+  ],
   // dont put the services here in the providers array.
   // it is better to mark the services with @Injectable and providedIn: 'root',
   // this will avoid problems related to multiple instances of the Services.
